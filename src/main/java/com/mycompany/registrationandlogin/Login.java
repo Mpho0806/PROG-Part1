d5f36292759baa0f5769d07a775497613c5b2111
Student <Student@LabVM2049939.2z5gm0vmcgoutev3oxsyfrwtoa.jnbx.internal.cloudapp.net>
2026-09-18 18:31:57 +0200

    edited the login class

--- src/main/java/com/mycompany/registrationandlogin/Login.java
+++ src/main/java/com/mycompany/registrationandlogin/Login.java
@@ -3,11 +3,66 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.registrationandlogin;
+import java.util.Scanner;
 
-/**
- *
- * @author Student
- */
 public class Login {
     
+
+    public static void main(String[]args) {
+        
+          Scanner input = new Scanner(System.in);
+
+        System.out.println("===== REGISTRATION =====");
+
+        System.out.print("Enter your first name: ");
+        String firstName = input.nextLine();
+
+        System.out.print("Enter your last name: ");
+        String lastName = input.nextLine();
+
+        System.out.print("Enter your username: ");
+        String username = input.nextLine();
+
+        System.out.print("Enter your password: ");
+        String password = input.nextLine();
+
+        System.out.print("Enter your South African cell phone number: ");
+        String cellPhone = input.nextLine();
+
+        Login user = new Login(
+                firstName,
+                lastName,
+                username,
+                password,
+                cellPhone
+        );
+
+        System.out.println();
+        System.out.println(user.registerUser());
+
+        if (user.checkUserName()
+                && user.checkPasswordComplexity()
+                && user.checkCellPhoneNumber()) {
+
+            System.out.println();
+            System.out.println("===== LOGIN =====");
+
+            System.out.print("Enter your username: ");
+            String loginUsername = input.nextLine();
+
+            System.out.print("Enter your password: ");
+            String loginPassword = input.nextLine();
+
+            boolean loginSuccessful =
+                    user.loginUser(loginUsername, loginPassword);
+
+            System.out.println(
+                    user.returnLoginStatus(loginSuccessful)
+            );
+        }
+
+        input.close();
+    }
 }
+    
+
